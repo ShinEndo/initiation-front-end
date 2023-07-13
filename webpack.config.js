e79d53aa5a672cd478eff1d61939873de80cc7b9
ShinEndo <endo@elplanning.co.jp>
2023-07-14 08:50:43 +0900

    webpack babel typescriptの導入

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    mode: process.env.NODE_ENV || 'development',
+    entry: './src/app.ts',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+            test: /\.(j|t)s$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                presets: [
+                    '@babel/preset-env',
+                    '@babel/preset-typescript',
+
+                ]
+                }
+            }
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    }
+}
